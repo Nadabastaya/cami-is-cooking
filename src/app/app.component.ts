c6b38a29a3d0994086de9ce804da692417eed87f
Nadabastaya <garcia.camilo@outlook.com>
2025-07-06 00:24:21 -0400

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,11 @@ import { CommonModule } from '@angular/common';
 import { ArticulosComponent } from './components/articulos/articulos.component';
 import { AntojosComponent } from "./components/antojos/antojos.component";
 import { PortadaComponent } from './components/portada/portada.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, RecetasRecientesComponent, ArticulosComponent, CommonModule, RouterModule, AntojosComponent, PortadaComponent],
+  imports: [RouterOutlet, NavbarComponent, RecetasRecientesComponent, ArticulosComponent, CommonModule, RouterModule, AntojosComponent, PortadaComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
